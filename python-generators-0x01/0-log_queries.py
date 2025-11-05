84f0ed430c1f8bf87f5378d95852fa850865e76d
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-05 14:44:14 +0000

    log_queries done

---
+++ python-generators-0x01/0-log_queries.py
@@ -0,0 +1,30 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+""" YOUR CODE GOES HERE"""
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if args:
+            query = args[0]
+        else:
+            query = kwargs.get('query', '')
+        print(f"Executing query: {query}")
+        result = func(*args, **kwargs)
+        print("Query executed successfully.")
+        return result
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
