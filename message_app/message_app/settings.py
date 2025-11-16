22848418f4413664f6cf453d04741cb56fa12993
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-16 12:49:21 +0000

    Added the rest_framework and the in the django project settings

--- message_app/message_app/settings.py
+++ message_app/message_app/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "rest_framework",
+    "chats",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
