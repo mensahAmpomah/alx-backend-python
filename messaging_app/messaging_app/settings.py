6a7347a116b78704963a54d77d619c33fb7838b0
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-16 13:12:16 +0000

    Changed the Rest_framework from Renderer to Authentication

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -126,9 +126,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES":[
-        "rest_framework.permission.AllowAny",
+        "rest_framework.permission.IsAuthenticated",
     ],
-    "DEFAULT_RENDERER_CLASSES": [
-        "rest_framework.renderers.JSONRendere",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+
     ],
 }
