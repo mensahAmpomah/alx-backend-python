96003da184f342165f03d644734a6dbb2285ae11
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-16 06:10:43 +0000

    http mock done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,33 @@
+#!/usr/bin/env python3
+from parameterized import parameterized
+import unittest
+from utils import access_nested_map
+from utils import get_json
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests for the access_nested_map function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map returns expected results"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        """Test KeyError is raised when path is invalid"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_key}'")
+
+
+
 class TestGetJson(unittest.TestCase):
     """Tests for utils.get_json"""
 
