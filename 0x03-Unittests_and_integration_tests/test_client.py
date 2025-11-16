fd83f3681b77f45b9f3c0e24ab2c496026f72c9d
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-16 07:22:59 +0000

    parameterized corrected

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unit and integration tests for client.GithubOrgClient"""
 import unittest
 from unittest.mock import patch, PropertyMock, Mock
-from parameterized import parameterized, parameterized_class
+from parameterized import parameterized_class
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
