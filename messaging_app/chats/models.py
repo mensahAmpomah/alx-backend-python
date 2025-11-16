2aafab04e0e0f5f836e319e053c2c8b99a686729
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-16 13:21:59 +0000

    Included the password

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -9,7 +9,6 @@ USER_ROLES = [
     ("admin", "Admin"),
 ]
 
-
 class User(AbstractUser):
     """Custom User model extending Django's AbstractUser"""
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
@@ -17,13 +16,10 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     role = models.CharField(max_length=10, choices=USER_ROLES, default="guest")
     created_at = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=128)
 
-    # Remove username if you prefer email login (optional)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.email})"
 
 
 class Conversation(models.Model):
