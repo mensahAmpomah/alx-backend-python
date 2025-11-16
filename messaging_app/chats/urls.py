1313248e29772885a4301abadd8affebf3b88050
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-16 14:09:50 +0000

    fixed the nestedurls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
-from rest_framework import routers  # import routers to use routers.DefaultRouter()
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
+
 from .views import ConversationViewSet, MessageViewSet
 
-# Create a DefaultRouter instance explicitly
-router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+# Main router
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversations')
 
-# Include all router URLs
+# Nested router: messages inside a conversation
+nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(nested_router.urls)),
 ]
