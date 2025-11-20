34e36a8b44a2a3719299c0c7accfe18280a79914
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-20 15:49:20 +0000

    Permissions Authenticated

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -133,6 +133,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.permissions.IsAuthenticated",
 
     ],
 }
