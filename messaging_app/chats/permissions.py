856a9c39b2c90670daee82a8442febe8a3069105
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-20 16:03:27 +0000

    Permission.BasePermission

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,14 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsOwner(BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only owners of an object to edit it.
+    """
+
     def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the object
         return obj.owner == request.user
-    
