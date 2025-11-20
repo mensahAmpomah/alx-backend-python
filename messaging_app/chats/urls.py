84a59ca90da603dbca857e8a512ad2be83228aba
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-20 15:43:41 +0000

    Installed the JWT

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
 
 from .views import ConversationViewSet, MessageViewSet
-
 # Main router
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
@@ -15,4 +14,5 @@ nested_router.register(r'messages', MessageViewSet, basename='conversation-messa
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(nested_router.urls)),
+    
 ]
