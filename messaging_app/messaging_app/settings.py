31c8f513cffb39c9e593890a7ea48074c6e881a0
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-21 10:44:37 +0000

    Pagination settings done

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -147,7 +147,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
-    "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+
     "PAGE_SIZE": 20,
 
     "DEFAULT_FILTER_BACKENDS": [
