cf3f59a3c7d41a2f61c8cfcb0f2d79f373ba2294
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-23 16:20:35 +0000

    Rolepermission

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,39 +1,19 @@
-import time
-from django.http import HttpResponse
+from django.http import HttpResponseForbidden
 
-class OffensiveLanguageMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # { ip_address : [timestamps_of_posts] }
-        self.message_history = {}
-
-        # Settings
-        self.TIME_WINDOW = 60     # 60 seconds = 1 minute
-        self.LIMIT = 5            # 5 messages per minute
 
     def __call__(self, request):
-        # Only track POST requests (messages being sent)
-        if request.method == "POST":
-            ip = request.META.get("REMOTE_ADDR")
-            now = time.time()
+        # Only check authenticated users
+        if request.user.is_authenticated:
+            # Assuming user has a 'role' attribute
+            role = getattr(request.user, "role", None)
+            if role not in ["admin", "moderator"]:
+                return HttpResponseForbidden("You do not have permission to perform this action.")
+        else:
+            # If user is not authenticated, also block
+            return HttpResponseForbidden("You do not have permission to perform this action.")
 
-            # Create entry for new IPs
-            if ip not in self.message_history:
-                self.message_history[ip] = []
-
-            # Remove timestamps older than 1 minute
-            self.message_history[ip] = [
-                t for t in self.message_history[ip] if now - t < self.TIME_WINDOW
-            ]
-
-            # Check if limit exceeded
-            if len(self.message_history[ip]) >= self.LIMIT:
-                return HttpResponse(
-                    "Message limit exceeded. Try again later.",
-                    status=429
-                )
-
-            # Add new message timestamp
-            self.message_history[ip].append(now)
-
-        return self.get_response(request)
+        response = self.get_response(request)
+        return response
