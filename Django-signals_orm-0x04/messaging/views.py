bba472d8ac9732b95329b8235631a391a9d381d6
mensahAmpomah <gideonmensah201@gmail.com>
2025-11-29 19:35:23 +0000

    delete_user view created

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+@login_required
+def delete_user(request):
+    """
+    Allows a logged-in user to delete their own account.
+    """
+
+    user = request.user
+
+    logout(request)          # log them out before deleting
+    user.delete()            # triggers post_delete signal
+
+    return redirect("home") 
